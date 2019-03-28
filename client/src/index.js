288f37faf54dd46fc8e632dff907c513d0906c01
MrT3313 <rturgeon13@gmail.com>
2019-03-27 20:17:58 -0500

    BrowserRouter imported & <App /> has been wrapped in <Router> in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// Import Dependencies
+    import React from 'react';
+    import ReactDOM from 'react-dom';
+    import { BrowserRouter as Router } from 'react-router-dom'
 
-import './index.css';
-import App from './App';
+// Import Components
+    import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Styling
+    import './index.css';
+
+
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
